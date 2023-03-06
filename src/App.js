3c8443b94f325f1b21179ac40fc60c22dd3a84af
Giovani Madero <gioaldo172@gmail.com>
2023-03-06 01:18:03 -0600

    Consumiendo Api, con React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react"        //Importando los hooks
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App =()=>{
+
+    const [res, cambiarres]=useState([]) 
+
+    useEffect(() => {                                       //Usando el hook para cuando cambie el estado, se renderize  el componente
+      fetch("https://jsonplaceholder.typicode.com/users")   //Usando la api de fetch
+      .then((response)=>response.json())                    //Convertimos la respuesta en formato JSON
+      .then((res)=>cambiarres(res))                         //Cambiamos el estado
+      .catch(error=>console.log(error))
+      
+      },[])
+    
+    return(
+        <div>
+            <h1>hola</h1>
+            <ul>
+                {res.map((r)=>{                             //usamos el metodo map para recorrer los objetos
+                    return(
+                        <li key={r.id}>{r.name}</li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
 }
 
-export default App;
+export default App
